12db447b1fb9c7db9e4cafb2a35b6f8380315896
Christian Nelson <zero@zerolives.org>
2010-11-16 16:57:09 -0500

    Version 1.0a

--- main.lua
+++ main.lua
@@ -1,19 +1,7 @@
 require('xf_message.lua')
 
-love.graphics.setFont(24)
+ef_addMessage('Hello!',250, 250, 'up')
 
 function love.draw()
-	xf_printMessages()
-	
---	love.graphics.print(#xf_messages, 50,50)
-	love.graphics.print(love.timer.getTime(), 100, 50)
---	love.graphics.print(timeElapsed .. ' - ' .. nowTime .. ' = ' .. (timeElapsed - nowTime), 50, 100)
-	
-	xf_nowTime = love.timer.getTime()
-
-	if (xf_timeElapsed - xf_nowTime) < 0 then
-		xf_addMessage('Triggered at ' .. xf_nowTime,250,250, 'left')
-		xf_timeElapsed = xf_nowTime+1
-	end
-	
+	ef_printMessages()	
 end
